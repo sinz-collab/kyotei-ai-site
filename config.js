fb2b3ef376ee785343289263ca5d67c821947528
sinz-collab <sinz1981@gmail.com>
2026-07-14 15:10:50 +0900

    Auto update site 20260714

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.KYOTEI_DATA_BASES =
   window.KYOTEI_DATA_BASES ||
   (location.protocol === "file:" || ["localhost", "127.0.0.1", "::1"].includes(location.hostname)
-    ? ["../../kyotei-ai-data/data", "https://raw.githubusercontent.com/sinz-collab/kyotei-ai-data/576397eb5e2ab7dd6d8ec0f57c26ea3187754b8a/data", "https://raw.githubusercontent.com/sinz-collab/kyotei-ai-data/main/data"]
-    : ["https://raw.githubusercontent.com/sinz-collab/kyotei-ai-data/576397eb5e2ab7dd6d8ec0f57c26ea3187754b8a/data", "https://raw.githubusercontent.com/sinz-collab/kyotei-ai-data/main/data"]);
+    ? ["../../kyotei-ai-data/data", "https://raw.githubusercontent.com/sinz-collab/kyotei-ai-data/0752c5dc5365f168f9574ed029e31d72e691ab95/data", "https://raw.githubusercontent.com/sinz-collab/kyotei-ai-data/main/data"]
+    : ["https://raw.githubusercontent.com/sinz-collab/kyotei-ai-data/0752c5dc5365f168f9574ed029e31d72e691ab95/data", "https://raw.githubusercontent.com/sinz-collab/kyotei-ai-data/main/data"]);
